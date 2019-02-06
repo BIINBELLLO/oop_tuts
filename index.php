a7df198f2dc3c99e1fcadb8b4d06a3c60544444a
BIINBELLLO <abdulmateenbinbello@gmail.com>
2019-02-06 12:21:32 +0100

    Tuts Class 7 Done

--- index.php
+++ index.php
@@ -12,8 +12,9 @@
 <?php
 
 	// $object->setNewData("This is the first user property");
-
+	// unset($object);
 	echo $object->getProperty(). "<br>";
+	
 
 	// $object_2->setNewData("This is the second user property");
 
