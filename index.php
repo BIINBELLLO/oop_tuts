ee7b1c10b991a0bec9139cba6e8d4ef894714350
BIINBELLLO <abdulmateenbinbello@gmail.com>
2019-02-06 11:53:58 +0100

    Tuts Continued

--- index.php
+++ index.php
@@ -8,7 +8,12 @@
 </head>
 <body>
 <?php
-	
+	echo $object->getProperty(). "<br>";
+
+	$object->setNewData("This is the new property");
+
+	echo $object->getProperty(). "<br>";
+
 ?>
 </body>
 </html>
